9460a7f718e8306b6adefeda8e9ee49640f3d681
username <wkadlf2310@gmail.com>
2022-05-19 14:34:32 +0900

    Change text of tv

--- app/src/main/java/smu/app/git_test/MainActivity.kt
+++ app/src/main/java/smu/app/git_test/MainActivity.kt
@@ -2,10 +2,13 @@ package smu.app.git_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textview = findViewById<TextView>(R.id.tv)
+        textview.text = "Changed in Kotlin"
     }
 }
